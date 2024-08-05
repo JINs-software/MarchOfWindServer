3a968853a2e7fba7c8eaf325d4ebaa6a5f8ad8c5
JinHur <jinh2352@gmail.com>
2024-08-06 02:44:28 +0900

    클라이언트의 싱글 스레드 로직 처리와 유사한 형태로 게임 오브젝트 추가 및 싱글 업데이트 스레드에 이동 로직을 수행하도록 구현 중...

---
+++ MarchOfWindServer/UpdateThread.cpp
@@ -0,0 +1,53 @@
+#include "UpdateThread.h"
+
+using namespace std;
+
+UINT __stdcall UpdateThread::UpdateThreadFunc(void* arg)
+{
+	UpdateThread* updateThread = reinterpret_cast<UpdateThread*>(arg);
+
+	clock_t timeStamp = clock();
+	while (true) {
+		// 1. 오브젝트 삭제 요청 수행
+		{
+			lock_guard<mutex> lockGuard(updateThread->m_ReadyToDestroyObjectsMtx);
+
+			for (auto iter = updateThread->m_GameObjects.begin(); iter != updateThread->m_GameObjects.end();) {
+				GameObject* gameObject = *iter;
+				if (updateThread->m_ReadyToDestroyObjects.find(gameObject) != updateThread->m_ReadyToDestroyObjects.end()) {
+					gameObject->OnDestroy();
+
+
+					updateThread->m_ReadyToDestroyObjects.erase(gameObject);
+					iter = updateThread->m_GameObjects.erase(iter);
+				}
+				else {
+					iter++;
+				}
+			}
+
+			if (updateThread->m_ReadyToDestroyObjects.size() > 0) {
+				DebugBreak();
+			}
+		}
+
+		// 2. 오브젝트 등록 요청 수행
+		{
+			lock_guard<mutex> lockGuard(updateThread->m_ReadyToRegistObjectsMtx);
+
+			for (auto gameObject : updateThread->m_ReadyToRegistObjects) {
+				updateThread->m_GameObjects.push_back(gameObject);
+			}
+
+			updateThread->m_ReadyToRegistObjects.clear();
+		}
+
+		// 3. Update
+		float deltaTime = (clock() - timeStamp) / static_cast<float>(CLOCKS_PER_SEC);
+		for (auto gameObject : updateThread->m_GameObjects) {
+			gameObject->OnUpdate(deltaTime);
+		}
+	}
+
+	return 0;
+}
